2f2597c1b1279ffd930a76915c356ce8798f6ebe
TEOS <proyectoteos@gmail.com>
2018-01-22 22:03:17 -0600

    Code and diagram available
    Files ready to carry out the project

---
+++ Sensor-de-vibracion-SW-18010P-con-Arduino.ino
@@ -0,0 +1,44 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+  WEB https://goo.gl/jtiYJy
+*/
+
+const int sensorPinT = 2;
+int timeStrokeT = 500;
+int startT, durationT;
+int strokeValT = HIGH;
+boolean stateT = false;
+unsigned long startStrokeT, finalStrokeT;
+
+void setup () {
+  Serial.begin(9600);
+  pinMode(sensorPinT, INPUT) ;
+}
+void loop() {
+  strokeValT = digitalRead(sensorPinT);
+  if (strokeValT == LOW) {
+    startStrokeT = millis();
+    if (!stateT) {
+      Serial.println("Vibracion detectada");
+      startT = startStrokeT / 1000;
+      stateT = true;
+    }
+  }
+  else {
+    if ((millis() - startStrokeT) > timeStrokeT && stateT) {
+      finalStrokeT = millis();
+      durationT = (finalStrokeT / 1000) - startT;
+      Serial.print("La vibracion ha finalizado con ");
+      Serial.print(durationT);
+      Serial.println(" segundos de duracion\n");
+      stateT = false;
+    }
+  }
+}
